f9b8bcc7a526714a4a609aee38d60dff3ca33d62
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-09 03:00:33 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,36 @@
+class Doctor
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|app| app.doctor == self}
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.collect {|appointment| appointment.patient}
+  end
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+end
